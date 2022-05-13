908ccb05a0dd4ea3666024f07768255dbb7e97c2
Rishav017439 <105158145+Rishav017439@users.noreply.github.com>
2022-05-13 16:43:21 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,22 +46,23 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>⏰ Bot Uptime:</b> {currentTime}\n\n'\
+    stats = f'<b>📊 Commit Date:</b> {last_commit}\n\n'\
+            f'<b>🤖 Bot Uptime:</b> {currentTime}\n'\
             f'<b>☢️ OS Uptime:</b> {osUptime}\n\n'\
-            f'<b>🗄️ Total Disk Space:</b> {total}\n'\
+            f'<b>📀 Total Disk Space:</b> {total}\n'\
             f'<b>🗃️ Used:</b> {used} | <b>📂 Free:</b> {free}\n\n'\
-            f'<b>⏫ Uploaded:</b> {sent}\n'\
-            f'<b>⏬ Downloaded:</b> {recv}\n\n'\
+            f'<b>🔼 Upload:</b> {sent}\n'\
+            f'<b>🔽 Download:</b> {recv}\n\n'\
             f'<b>🖥️ CPU:</b> {cpuUsage}%\n'\
             f'<b>⛏️ RAM:</b> {mem_p}%\n'\
             f'<b>🗄️ DISK:</b> {disk}%\n\n'\
-            f'<b>💠 Physical Cores:</b> {p_core}\n'\
+            f'<b>🔌 Physical Cores:</b> {p_core}\n'\
             f'<b>🍿 Total Cores:</b> {t_core}\n\n'\
-            f'<b>🎗️ SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>💾 Memory Total:</b> {mem_t}\n'\
-            f'<b>🕳️ Memory Free:</b> {mem_a}\n'\
-            f'<b>🧠 Memory Used:</b> {mem_u}\n'
-    sendMessage(stats, context.bot, update)
+            f'<b>🎗️ SWAP:</b> {swap_t} | <b>🔱 Used:</b> {swap_p}%\n'\
+            f'<b>🤖 Memory Total:</b> {mem_t}\n'\
+            f'<b>🧠 Memory Free:</b> {mem_a}\n'\
+            f'<b>🕳️ Memory Used:</b> {mem_u}\n'
+    sendMessage(stats, context.bot, update.message)
 
 
 def start(update, context):
